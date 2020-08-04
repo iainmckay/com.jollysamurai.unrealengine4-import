4921697abc4e37327e6ad2f4af494588384cc35c
Iain Mckay <me@iainmckay.co.uk>
2020-08-04 20:31:28 +0200

    Added connections for Fresnel node

--- Editor/ShaderGraph/Converters/Expressions/MaterialExpressionFresnelConverter.cs
+++ Editor/ShaderGraph/Converters/Expressions/MaterialExpressionFresnelConverter.cs
@@ -4,6 +4,8 @@ using JollySamurai.UnrealEngine4.T3D.Material;
 using JollySamurai.UnrealEngine4.T3D.Parser;
 using UnityEditor.ShaderGraph;
 using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using Material = JollySamurai.UnrealEngine4.T3D.Material.Material;
 
 namespace JollySamurai.UnrealEngine4.Import.ShaderGraph.Converters.Expressions
 {
@@ -24,6 +26,21 @@ namespace JollySamurai.UnrealEngine4.Import.ShaderGraph.Converters.Expressions
         public override int GetConnectionSlotId(AbstractMaterialNode from, AbstractMaterialNode to, int toSlotId, ParsedPropertyBag propertyBag)
         {
             return 3;
+        }
+
+        public override void CreateConnections(MaterialExpressionFresnel unrealNode, Material unrealMaterial, ShaderGraphBuilder builder)
+        {
+            if(unrealNode.ExponentIn != null) {
+                builder.Connect(unrealNode.ExponentIn, unrealNode.Name, 0);
+            }
+
+            if(unrealNode.BaseReflectFractionIn != null) {
+                Debug.Log("FIXME: unhandled fresnel baseReflectFractionIn");
+            }
+
+            if(unrealNode.Power != null) {
+                builder.Connect(unrealNode.Power, unrealNode.Name, 2);
+            }
         }
     }
 }
